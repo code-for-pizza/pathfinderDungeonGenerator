232cd32e09f73471a64167b4bb20de13b5b55673
Holidaez <holidaez@KC>
2024-03-14 10:54:49 -0400

    KA moved data to a exterior folder for easier reusability

--- fontend-app/src/components/SelectedStyle.js
+++ fontend-app/src/components/SelectedStyle.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import {Form} from 'react-bootstrap';
-
+import { style } from './datasets';
 function SelectStyle({setSetter}) {
     return (
                                 <Form.Select name="selectedStyle" onChange={e => setSetter(e.target.value)}>
                                     <option>Style Selection</option>
-                                    <option value ="Dungeon Crawl">Dungeon Crawl</option>
-                                    <option value ="Gritty Adventure">Gritty Adventure</option>
-                                    <option value ="High Adventure">High Adventure</option>
-                                    <option value ="Horror">Horror</option>
-                                    <option value ="Intrigue"> Intrigue</option>
-                                    <option value ="Military Adventure">Military Adventure</option>
-                                    <option value ="Mystery">Mystery</option>
-                                    <option value ="Planar Adventure">Planar Adventure</option>
-                                    <option value ="Romantic Adventure">Romantic Adventure</option>
+                                    {style.map (styl => (
+                                        <option value={styl}>{styl}</option>
+                                    ))}
                                 </Form.Select>
     );
 };
